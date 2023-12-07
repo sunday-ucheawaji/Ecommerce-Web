939fbb0a777729be5796a21d7317f7cdf3650cf8
Sunday Ucheawaji <sundayucheawaji@gmail.com>
2023-12-07 16:31:53 +0100

    configure CustomUserDbContext

---
+++ Data/CustomUserDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceWeb.Data
+{
+    public class CustomUserDbContext:IdentityDbContext
+    {
+        public CustomUserDbContext(DbContextOptions<CustomUserDbContext> options): base(options)
+        {
+            
+        }
+    }
+}
